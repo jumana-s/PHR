c893bd848563fdb588bbe9b1b749ecedc8f0ca60
jumana-s <salwam4@gmail.com>
2023-03-21 09:05:35 -0400

    chore: fix js files formatting

--- my-app/src/Drawer.js
+++ my-app/src/Drawer.js
@@ -1,159 +1,162 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { styled } from '@mui/material/styles';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import AddModeratorIcon from '@mui/icons-material/AddModerator';
-import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
-import PeopleIcon from '@mui/icons-material/People';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import MenuIcon from '@mui/icons-material/Menu'
+import * as React from "react";
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AddModeratorIcon from "@mui/icons-material/AddModerator";
+import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
+import PeopleIcon from "@mui/icons-material/People";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import MenuIcon from "@mui/icons-material/Menu";
 import axios from "axios";
 
 const drawerWidth = 240;
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    '& .MuiDrawer-paper': {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
+        duration: theme.transitions.duration.leavingScreen,
       }),
-      boxSizing: 'border-box',
-      ...(!open && {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
-        },
-      }),
-    },
-  }),
-);
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-  })(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    ...(open && {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-  }));
+  }),
+}));
 
 const DashboardDrawer = (props) => {
-    const [open, setOpen] = useState(true);
-    const toggleDrawer = () => {
-        setOpen(!open);
-    };
-    
-    function logMeOut() {
-        axios({
-            method: "POST",
-            url:"/logout",
-        })
-        .then((response) => {
-          props.removeToken()
-          props.removeId()
-          window.location.href = '/';
-        }).catch((error) => {
-            if (error.response) {
-            console.log(error.response)
-            console.log(error.response.status)
-            console.log(error.response.headers)
-            }
-        })
-    }
+  const [open, setOpen] = useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
-    return (
-        <React.Fragment>
-            <AppBar position="absolute" open={open}>
-                <Toolbar
-                    sx={{
-                        pr: '24px',
-                    }}
-                >
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={toggleDrawer}
-                    sx={{
-                        marginRight: '36px',
-                        ...(open && { display: 'none' }),
-                    }}
-                    >
-                    <MenuIcon />
-                </IconButton>
-                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-                        PHR System
-                    </Typography>
-                    <Button color="inherit" variant="outlined" onClick={logMeOut}>Logout</Button>
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-            <Toolbar
-                sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'flex-end',
-                px: [1],
-                }}
-            >
-                <IconButton onClick={toggleDrawer}>
-                <ChevronLeftIcon />
-                </IconButton>
-            </Toolbar>
-            <Divider />
-                <List component="nav">
-                    <ListItemButton onClick={() => props.setTab('phr')}>
-                        <ListItemIcon>
-                            <MedicalInformationIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="My PHR" />
-                    </ListItemButton>
+  function logMeOut() {
+    axios({
+      method: "POST",
+      url: "/logout",
+    })
+      .then((response) => {
+        props.removeToken();
+        props.removeId();
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
 
-                    <ListItemButton onClick={() => props.setTab('access')}>
-                        <ListItemIcon>
-                            <AddModeratorIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Set PHR Access" />
-                    </ListItemButton>
+  return (
+    <React.Fragment>
+      <AppBar position="absolute" open={open}>
+        <Toolbar
+          sx={{
+            pr: "24px",
+          }}
+        >
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: "36px",
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            PHR System
+          </Typography>
+          <Button color="inherit" variant="outlined" onClick={logMeOut}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            px: [1],
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          <ListItemButton onClick={() => props.setTab("phr")}>
+            <ListItemIcon>
+              <MedicalInformationIcon />
+            </ListItemIcon>
+            <ListItemText primary="My PHR" />
+          </ListItemButton>
 
-                    <ListItemButton onClick={() => props.setTab('other')}>
-                        <ListItemIcon>
-                            <PeopleIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Other PHRs" />
-                    </ListItemButton>
-                </List>
-            </Drawer>
-        </React.Fragment>    
+          <ListItemButton onClick={() => props.setTab("access")}>
+            <ListItemIcon>
+              <AddModeratorIcon />
+            </ListItemIcon>
+            <ListItemText primary="Set PHR Access" />
+          </ListItemButton>
+
+          <ListItemButton onClick={() => props.setTab("other")}>
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Other PHRs" />
+          </ListItemButton>
+        </List>
+      </Drawer>
+    </React.Fragment>
   );
-}
+};
 
 export default DashboardDrawer;
--- my-app/src/EditAccess.js
+++ my-app/src/EditAccess.js
@@ -1,102 +1,171 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import axios from 'axios';
-import Alert from '@mui/material/Alert';
-import Typography from '@mui/material/Typography';
+import axios from "axios";
+import Alert from "@mui/material/Alert";
+import Typography from "@mui/material/Typography";
 
-import AddButtonGroup from './AddButtonGroup';
+import AddButtonGroup from "./AddButtonGroup";
 
-import { facility, facilityName, department, position, symbols, keys } from './lists';
+import {
+  facility,
+  facilityName,
+  department,
+  position,
+  symbols,
+  keys,
+} from "./lists";
 
 const EditAccess = (props) => {
-    const [array, updateArray] = useState([]);
-    const [isReqSuccess, setIsReqSuccess] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
+  const [array, updateArray] = useState([]);
+  const [isReqSuccess, setIsReqSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    function handleSubmit(event) {
+  function handleSubmit(event) {
+    // create proper access tree
+    let access = "( " + props.id + " OR ( ";
 
-        // create proper access tree
-        let access = '( '+props.id+' or ( ';
+    for (let i = 0; i < array.length; i++) {
+      if (
+        array[i] != "(" &&
+        array[i] != "AND" &&
+        array[i] != "OR" &&
+        array[i] != ")"
+      ) {
+        access += keys[array[i]];
+      } else {
+        access += array[i];
+      }
 
-        for(let i = 0; i < array.length; i++) {
-            if(array[i] !== "(" && array[i] !== "AND" && array[i] !== "OR" && array[i] !== ")") {
-                access += keys[array[i]];
-            } else {
-                access += array[i];
-            }
+      access += " ";
+    }
 
-            access += ' ';
+    access += " ))";
+
+    axios({
+      method: "POST",
+      url: "/access",
+      data: {
+        list: access,
+        id: props.id,
+      },
+      headers: {
+        Authorization: "Bearer " + props.token,
+      },
+    })
+      .then((response) => {
+        setIsReqSuccess(true);
+        console.log(response.data.msg);
+        // window.location.href = '/dashboard';
+      })
+      .catch((error) => {
+        if (error.response) {
+          setIsReqSuccess(false);
+          setErrorMessage(error.response.data.msg || error.response.statusText);
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
         }
+      });
 
-        access += ' ))';
+    updateArray([]);
 
-        axios({
-            method: "POST",
-            url:"/access",
-            data:{
-                list: access,
-                id: props.id,
-            },
-            headers: {
-                Authorization: 'Bearer ' + props.token
-            }
-        })
-        .then((response) => {
-            setIsReqSuccess(true)
-            console.log(response.data.msg)
-            // window.location.href = '/dashboard';
-        }).catch((error) => {
-            if (error.response) {
-                setIsReqSuccess(false)
-                setErrorMessage(error.response.data.msg || error.response.statusText)
-                console.log(error.response)
-                console.log(error.response.status)
-                console.log(error.response.headers)
-            }
-        })
+    event.preventDefault();
+  }
 
-        updateArray([]);
-    
-        event.preventDefault()
-    };
-    
-    return (
-        <Box m="auto" align="center" component="form" onSubmit={handleSubmit} sx={{ width: 3 / 4 }}>
-            {isReqSuccess ?
-                <Alert variant="outlined" severity="success" sx={{mb: 3}}> Attributes to access your PHR were updated! </Alert>
-            :
-            errorMessage.length > 0 && <Alert variant="outlined" severity="error" sx={{mb: 3}}>{errorMessage}</Alert>
-            }
+  return (
+    <Box
+      m="auto"
+      align="center"
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ width: 3 / 4 }}
+    >
+      {isReqSuccess ? (
+        <Alert variant="outlined" severity="success" sx={{ mb: 3 }}>
+          {" "}
+          Attributes to access your PHR were updated!{" "}
+        </Alert>
+      ) : (
+        errorMessage.length > 0 && (
+          <Alert variant="outlined" severity="error" sx={{ mb: 3 }}>
+            {errorMessage}
+          </Alert>
+        )
+      )}
 
-            <Typography variant="h4" gutterBottom> Edit Access of Your PHR</Typography>
+      <Typography variant="h4" gutterBottom>
+        {" "}
+        Edit Access of Your PHR
+      </Typography>
 
-            <TextField fullWidth sx={{ mt: 5, mb: 5 }}
-                id="read-only-input"
-                label="Access Policy"
-                name="list"
-                value={array.join(' ')}
-                InputProps={{
-                    readOnly: true,
-                }}
-                variant="filled"
-            />
+      <TextField
+        fullWidth
+        sx={{ mt: 5, mb: 5 }}
+        id="read-only-input"
+        label="Access Policy"
+        name="list"
+        value={array.join(" ")}
+        InputProps={{
+          readOnly: true,
+        }}
+        variant="filled"
+      />
 
-            <AddButtonGroup options={facility} id="facility-type" name="Facility Type" updateArray={updateArray} />
-            <AddButtonGroup options={facilityName} id="facility-name" name="Facility Name" updateArray={updateArray}/>
-            <AddButtonGroup options={department} id="dept" name="Department Name" updateArray={updateArray}/>
-            <AddButtonGroup options={position} id="position" name="Position Type" updateArray={updateArray} />
-            <AddButtonGroup options={symbols} name="Symbols" updateArray={updateArray} />
-            
-            {/* <hr /> */}
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', m:5}}>
-                <Button color="secondary" variant="outlined" onClick={() => updateArray([])} sx={{mr:3}}>Clear</Button>
-                <Button color="secondary" type="submit" variant="contained">Submit</Button>
-            </Box>
+      <AddButtonGroup
+        options={facility}
+        id="facility-type"
+        name="Facility Type"
+        updateArray={updateArray}
+      />
+      <AddButtonGroup
+        options={facilityName}
+        id="facility-name"
+        name="Facility Name"
+        updateArray={updateArray}
+      />
+      <AddButtonGroup
+        options={department}
+        id="dept"
+        name="Department Name"
+        updateArray={updateArray}
+      />
+      <AddButtonGroup
+        options={position}
+        id="position"
+        name="Position Type"
+        updateArray={updateArray}
+      />
+      <AddButtonGroup
+        options={symbols}
+        name="Symbols"
+        updateArray={updateArray}
+      />
 
-        </Box>
-    );
-}
+      {/* <hr /> */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          m: 5,
+        }}
+      >
+        <Button
+          color="secondary"
+          variant="outlined"
+          onClick={() => updateArray([])}
+          sx={{ mr: 3 }}
+        >
+          Clear
+        </Button>
+        <Button color="secondary" type="submit" variant="contained">
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
 export default EditAccess;
